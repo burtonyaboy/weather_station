df4b4f94272260a33309de9c870f50e209833bae
burtonyaboy <32086278+burtonyaboy@users.noreply.github.com>
2019-04-22 23:05:05 -0500

    Added a basic receiver script

---
+++ receiver.ino
@@ -0,0 +1,29 @@
+/*
+* Receiver code stolen from a nice person on the internet. 
+* Only temporary until the Raspberry Pi code is ready.
+* Thanks dude!
+*/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // CE, CSN
+
+const byte address[6] = "00001";
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+
+void loop() {
+  if (radio.available()) {
+    char text[256];
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+  }
+}
